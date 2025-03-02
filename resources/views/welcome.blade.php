c425df3d9047677ab970b704e47ed115822afcfe
Jean Paul Briffa <jean.briffa.i79575@mcast.edu.mt>
2025-03-02 15:39:50 +0000

    10-Welcome_Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,8 @@
 @extends('layouts.main')
+@section('content')
+    <div class="welcome-container">
+        <h1 class="welcome-title">Welcome to the College & Student Management System</h1>
+        <p class="welcome-text">Made by Jean Paul Briffa</p>
+        <p class="welcome-text">SWD-6.2A</p>
+    </div>
+@endsection
